400745bf6f97df34f6e4348c03214c2a87f0d9f4
Fara Akhatova <faraakhatova@gmail.com>
2021-11-08 18:20:53 -0700

    Add Article component

---
+++ src/Components /Article/Article.js
@@ -0,0 +1,14 @@
+
+
+const Article = () => {
+    return (
+        <main className='article-container'>
+            <article className='title-container'>
+
+            </article>
+            <article className='image-container'>
+                
+            </article>
+        </main>
+    )
+}
