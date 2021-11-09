915de468cc1cd21f5d724fb2d59624a00351cc82
Fara Akhatova <faraakhatova@gmail.com>
2021-11-09 10:41:50 -0700

    Move query fetch to App component

--- src/Components /Article/Article.js
+++ src/Components /Article/Article.js
@@ -1,22 +1,6 @@
-import { useEffect, useState } from "react";
-import { nyTimesData } from "../../API/ApiCalls";
-import { useQuery } from "react-query";
+
 
 const Article = () => {
-    const [topArticles, setTopArticles] = useState(null)
-
-    const { isLoading, error, data, isFetching } = useQuery("HomeData", () =>  nyTimesData.fetchAlldata())
-
-    useEffect(() => {
-    
-    if (isLoading) return "Loading...";
-
-    if (error) return "An error has occurred: " + error.message;
-
-        setTopArticles(data.results)
-
-    }, [])
-
     return (
         <main className='article-container'>
             <article className='title-container'>
