82451f3742d4785fcd705471387dc8f3d448f402
Fara Akhatova <faraakhatova@gmail.com>
2021-11-09 10:35:24 -0700

    Add fetched data to a state

--- src/Components /Article/Article.js
+++ src/Components /Article/Article.js
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { nyTimesData } from "../../API/ApiCalls";
-
+import { useQuery } from "react-query";
 
 const Article = () => {
+    const [topArticles, setTopArticles] = useState(null)
 
+    const { isLoading, error, data, isFetching } = useQuery("HomeData", () =>  nyTimesData.fetchAlldata())
 
     useEffect(() => {
-        // nyTimesData.fetchAlldata().then(data => console.log(data))
-    }, [])
+    
+    if (isLoading) return "Loading...";
 
+    if (error) return "An error has occurred: " + error.message;
+
+        setTopArticles(data.results)
+
+    }, [])
 
     return (
         <main className='article-container'>
