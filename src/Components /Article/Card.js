feac689ab8874900c0adf9286b62e2d380feee8d
Fara Akhatova <faraakhatova@gmail.com>
2021-11-09 12:39:20 -0700

    Add article abstract

--- src/Components /Article/Card.js
+++ src/Components /Article/Card.js
@@ -5,8 +5,11 @@ const Card = ({article}) => {
         <>
         <article className='title-container'>
                 <h2 className='article-title'>
-                {article.title}
+                    {article.title}
                 </h2>
+                <p className='article-intro'>
+                    {article.abstract}
+                </p>
             </article>
             <article className='image-container'>
             <img src={article.multimedia[0].url} alt={article.multimedia[0].type}/>
