4edb9494708458b7bb1b59d9891a8db9ee2aaebd
Fara Akhatova <faraakhatova@gmail.com>
2021-11-10 08:37:44 -0700

    Add categories for articles

--- src/Components /NavBar/NavBar.js
+++ src/Components /NavBar/NavBar.js
@@ -1,12 +1,29 @@
 import './NavBar.css'
 
 const NavBar = () => {
+
+    const buttons = () => {
+        const categories = [
+            'arts', 'automobiles', 'books', 'business', 
+            'fashion', 'food', 'health', 'home', 
+            'insider', 'magazine', 'movies','nyregion',
+            'obituaries', 'opinion', 'politics', 'realestate',
+            'science', 'sports', 'sundayreview', 'technology', 
+            'theater', 't-magazine', 'travel', 'upshot', 'us', 'world'
+        ]
+
+    }
     return (
         <header className='header'>
             <nav className='navigation'>
                 <h1 className='title'>The New York Times</h1>
-                <input className='searchbar' type='search'>
-                </input>
+                <button>arts</button>
+                <button>automobiles</button>
+                <button>books</button>
+                <button>business</button>
+                <button>fashion</button>
+                <button>food</button>
+                <button></button>
             </nav>
         </header>
     )
